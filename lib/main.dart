626f452daea1157717d8e5d8725e5b7f8894d497
KoukiIijima24 <koki.iijima@jbs.com>
2023-11-25 21:37:08 +0900

    divide file

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:instagram_clone/view/home_view.dart';
 
 void main() {
   runApp(const MyApp());
@@ -21,21 +22,5 @@ class MyApp extends StatelessWidget {
         title: 'Flutter Demo',
       ),
     );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold();
   }
 }
